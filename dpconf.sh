c0846a1e31933ba87376bb7e7e4b735f75069ea3
Rafael Rodrigues <rafaelrodripaz@gmail.com>
2023-01-17 21:30:16 -0300

    Update dpconf.sh

--- dpconf.sh
+++ dpconf.sh
@@ -16,11 +16,10 @@ do
 for file in $(ls -la ./all_packages/$target/*)
 do
 
-for final_file in $(ls -la ./all_packages/$target/$file/*)
-do
-mv ./all_packages/$target/$file/$final_file ./all_packages/$target/
+
+mv ./all_packages/$target/$file/*.body ./all_packages/$target/
 rm -r ./all_packages/$target/$file
-mv ./all_packages/$target/$final_file ./all_packages/$target/$file
+mv ./all_packages/$target/*.body ./all_packages/$target/$file
 
 done
 done
