b033f2204c3a7e36475e6844da39790737a8a583
Rafael Rodrigues <rafaelrodripaz@gmail.com>
2023-01-17 21:19:07 -0300

    Update dpconf.sh

--- dpconf.sh
+++ dpconf.sh
@@ -11,6 +11,19 @@ echo "$(wc -l $inputfile) URLS"
 #Download de cada package.json utilizando o fff
 cat hosts-targets.txt | .fff -o ./all_packages -s 200
 
+for target in $(ls -la ./all_packages/*)
+do
+for file in $(ls -la ./all_packages/$target/*)
+do
+
+for final_file in $(ls -la ./all_packages/$target/$file/*)
+do
+mv ./all_packages/$target/$file/$final_file ./all_packages/$target/
+rm -r ./all_packages/$target/$file
+mv ./all_packages/$target/$final_file ./all_packages/$target/$file
+
+done
+done
 
 #Start package.json validation
 echo "Starting Dustilock analysis"
