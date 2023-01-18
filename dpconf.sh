06adf526f75c2abe001b24424cdb54ed08aa32c3
Rafael Rodrigues <rafaelrodripaz@gmail.com>
2023-01-17 21:04:33 -0300

    Update dpconf.sh

--- dpconf.sh
+++ dpconf.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
-while getopts p:i: flag
+while getopts i: flag
 do
     case "${flag}" in
-        p) path=${OPTARG};;
         i) inputfile=${OPTARG};;
     esac
 done
-echo "Scanning directory: $path";
 echo "Filename: $inputfile";
 echo "$(wc -l $inputfile) URLS"
 
 #Download de cada package.json utilizando o fff
-cat hosts-targets.txt | fff -o $path
+cat hosts-targets.txt | .fff -o ./all_packages -s 200
 
-#Talvez a rotina a seguir nao seja necessaria
-for url in $(cat $inputfile | sort -u):
-do
-domain_name=$(echo "$url" | sed -e 's|^[^/]*//||' -e 's|/.*$||')
-mkdir $path/$domain_name
-done
 
 #Start package.json validation
 echo "Starting Dustilock analysis"
-./Dustilock -p ./$path/ -r
+./Dustilock -p ./all_packages -r
