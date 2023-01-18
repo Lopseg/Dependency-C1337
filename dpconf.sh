bc1f392d9778cda8a5f487bd02d36e8a99ae6312
Rafael Rodrigues <rafaelrodripaz@gmail.com>
2023-01-17 23:47:04 -0300

    Update dpconf.sh

--- dpconf.sh
+++ dpconf.sh
@@ -8,18 +8,20 @@ done
 echo "Filename: $inputfile";
 echo "$(wc -l $inputfile) URLS"
 
+mkdir all_packages
+
+
 #Download de cada package.json utilizando o fff
 cat $inputfile | ./fff -o ./all_packages -s 200
 
-for target in $(ls -la ./all_packages/*)
+for target in $(ls -la ./all_packages/ | grep -v ' .'$ | grep -v ' ..'$)
 do
-for file in $(ls -la ./all_packages/$target/*)
+for file in $(ls -la ./all_packages/$target/ | grep -v ' .'$ | grep -v ' ..'$)
 do
 
-
-mv ./all_packages/$target/$file/*.body ./all_packages/$target/
+mv ./all_packages/$target/$file/*.body ../
 rm -r ./all_packages/$target/$file
-mv ./all_packages/$target/*.body ./all_packages/$target/$file
+mv ./all_packages/$target/*.body ../
 
 done
 done
